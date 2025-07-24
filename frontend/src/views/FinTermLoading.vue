5c310afdbc7c67cd1c2fcf31ff1e2f646f914691
hetao1 <hetao0921@126.com>
2025-07-24 19:06:40 +0800

    好好学习，天天向上

--- frontend/src/views/FinTermLoading.vue
+++ frontend/src/views/FinTermLoading.vue
@@ -61,7 +61,7 @@
 
     <el-divider />
     <h2>已加载文件列表 <span v-if="loadedFiles.length">（共 {{ loadedFiles.length }} 条）</span></h2>
-    <el-table :data="loadedFiles" style="width: 100%" v-loading="loadingList">
+    <el-table :data="loadedFiles.slice(0, 10)" style="width: 100%" v-loading="loadingList">
       <el-table-column prop="name" label="文件名" />
       <el-table-column prop="insert_time" label="加载时间" />
       <el-table-column prop="update_time" label="更新时间" />
